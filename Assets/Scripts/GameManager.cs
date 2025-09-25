0d1165641775f0b3bb515dce14f30bc01d8cc40d
Shon11215 <shon11215@gmail.com>
2025-09-25 22:54:10 +0300

    Last talis commit
    in this commit the spawn method works but not coin system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,26 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject enemy;
     public int enemyScore = 10;
+    public GameObject area;
+    //[SerializeField] float radius = 15f;
+    Vector2 randomDir;
+    Vector3 spawnPos;
+
     void Start()
     {
-
+        Bounds bounds =  area.GetComponent<Renderer>().bounds;
+        float spawnRadius = MathF.Max(bounds.extents.x, bounds.extents.y) + 5f ;
+        //spawnPos = bounds.center + (Vector3)randomDir * spawnRadius;
+        for(int i = 0; i < 4; i++)
+        {
+            randomDir = UnityEngine.Random.insideUnitCircle.normalized;
+            spawnPos = bounds.center + (Vector3)randomDir * spawnRadius;
+            Instantiate(enemy, spawnPos, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
